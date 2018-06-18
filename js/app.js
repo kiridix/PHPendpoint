e870de82bee32d5f48e257d66cf175b101cd57af
kiridix <matimar1414@gmail.com>
2018-06-18 14:48:26 -0300

    Added restriction to the ingressed values

--- js/app.js
+++ js/app.js
@@ -4,28 +4,38 @@ $().ready(function(){
         var value2 = $("#value2").val();
         var op = $("#operation").val();
 
-
-        var js = {values: {x: value1, y: value2}, operation: op};
-        console.log(js.values);
-        var js = JSON.stringify(js);
-    $.ajax({
-        type: "POST",
-        url: "api.php",
-        datatype: "string",
-        data: "request=" + js,
-        success: function(data) {
-            var response = JSON.parse(data);
-            if(response.status != "ERROR"){
-                $("#result").text("Result: "+response.result);
-            }else{
-                $("#result").text("ERROR");
+        if(check(value1) && check(value2)){
+            var js = {values: {x: value1, y: value2}, operation: op};
+            console.log(js.values);
+            var js = JSON.stringify(js);
+         $.ajax({
+            type: "POST",
+            url: "api.php",
+            datatype: "string",
+            data: "request=" + js,
+            success: function(data) {
+                var response = JSON.parse(data);
+                if(response.status != "ERROR"){
+                    $("#result").text("Result: "+response.result);
+                }else{
+                    $("#result").text("ERROR");
+                }
             }
+        });
+        }else{
+            $("#result").text("Please just enter numeric values");
         }
-    });
 
 
 
 
     })
+    function check(val){
+    if(!isNaN(val) && val.trim() != ""){
+        return true;
+    }
+    return false;
+} 
+
 
 });
